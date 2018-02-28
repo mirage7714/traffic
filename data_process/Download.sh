7d5c6af2204f5efd3d0a557a6c454f898136af2d
Mirage Chung <mirage7714@gmail.com>
2018-02-28 10:42:24 +0800

    Change the link address

--- data_process/Download.sh
+++ data_process/Download.sh
@@ -3,7 +3,7 @@ main=/home/hadoop/taipei
 
 cd ${main}
 
-wget https://tcgbusfs.blob.core.windows.net/blobtisv/GetVD.gz
-gzip -d GetVD.gz
-python3 dataProcess.py GetVD
+wget 	https://tcgbusfs.blob.core.windows.net/blobtisv/GetVD.xml.gz
+gzip -d GetVD.xml.gz
+python3 dataProcess.py GetVD.xml
 rm GetVD
